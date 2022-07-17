43e17e78ab83a8efc6525e62db14690e435391fe
Charlie <64078329+zacharlie@users.noreply.github.com>
2022-07-17 17:20:14 +0200

    update cleanups

--- geodata/cleanup.sh
+++ geodata/cleanup.sh
@@ -5,7 +5,7 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $THISDIR
 
 # remove files
-find . \( -name "test" -o -name "seed" \) -prune -o -type f \( -name "*.py" -o -name "*.qgs" -o -name "*.qgz" -o -name ".zip" \) -print0 | xargs -0 rm -vf
+find . \( -name "test" -o -name "seed" \) -prune -o -type f \( -name "*.py" -o -name "*.qgs" -o -name "*.qgz" -o -name ".zip" -o -name ".geojson" \) -print0 | xargs -0 rm -vf
 
 # Remove empty directories
 find . \( -name "test" -o -name "seed" \) -prune -o -type d -empty -print0 | xargs -0 -r rmdir -v
