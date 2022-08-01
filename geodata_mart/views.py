f9448b55c4a58e4ac0698dd447b5961bfe71f8ed
Charlie <64078329+zacharlie@users.noreply.github.com>
2022-08-01 15:48:44 +0200

    fix UnicodeDecodeError on gpkg

--- geodata_mart/views.py
+++ geodata_mart/views.py
@@ -4,8 +4,13 @@ from django.contrib.auth.decorators import login_required
 
 import mimetypes
 from pathlib import Path
+from urllib.parse import unquote
 from django.conf import settings
 from geodata_mart.maps.models import project_storage
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @login_required
@@ -14,7 +19,7 @@ def geodata(request, path):
         BASE_DIR = Path(settings.QGIS_DATA_ROOT)
         protected_paths = ["projects", "processing", "test"]
         protected_paths = [BASE_DIR / protected for protected in protected_paths]
-        filepath = BASE_DIR / Path(project_storage.path(path))
+        filepath = BASE_DIR / Path(project_storage.path(unquote(path)))
         filename = filepath.name
 
         is_protected = False
@@ -28,8 +33,11 @@ def geodata(request, path):
         if project_storage.exists(filepath):
             result = project_storage.open(filepath, "rb")
             mime_type, _ = mimetypes.guess_type(filepath)
+            if not mime_type:
+                mime_type = "text/plain"
             response = HttpResponse(result, content_type=mime_type)
             response["Content-Disposition"] = f"attachment; filename={filename}"
+            logger.info(f"{filename} downloaded by {request.user.id}")
             return response
         else:
             raise Http404("File does not exist")
